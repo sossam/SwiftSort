e729ac5b1488c05478b0aabfc6203711aebdf07d
sossam <heart_b_e@naver.com>
2021-01-13 20:39:55 +0900

    ✨ 선택 정렬 구현

--- SwiftSorted/main.swift
+++ SwiftSorted/main.swift
@@ -19,3 +19,15 @@ func bubbleSort(_ array: inout [Int]) {
         if isSwap == false { return }
     }
 }
+
+func selectionSort(_ array: inout [Int]) {
+    for stand in 0..<(array.count - 1) {
+        var minIndex = stand
+        for index in (stand + 1)..<array.count {       
+            if array[index] < array[minIndex] {
+                minIndex = index
+            }
+        }
+        array.swapAt(stand, minIndex)
+    }
+}

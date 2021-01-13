4899bae5e56df6c18c39ebe19190dc4e0284cce7
sossam <heart_b_e@naver.com>
2021-01-13 20:39:28 +0900

    ✨ 버블 정렬 구현

--- SwiftSorted/main.swift
+++ SwiftSorted/main.swift
@@ -7,4 +7,15 @@
 
 import Foundation
 
-
+func bubbleSort(_ array: inout [Int]) {
+    for index1 in 0..<(array.count - 1) {
+        var isSwap = false
+        for index2 in 0..<(array.count - 1 - index1) {
+            if array[index2] > array[index2 + 1] {
+                array.swapAt(index2, (index2 + 1))
+                isSwap = true
+            }
+        }
+        if isSwap == false { return }
+    }
+}
